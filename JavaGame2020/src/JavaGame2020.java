6ba9426cd43dab86bf2ba053eede461f6cb27f14
Thilovon-Trotha <Thilovon-Trotha@0-208-13>
2020-12-01 15:14:38 +0100

    hi

---
+++ JavaGame2020/src/JavaGame2020.java
@@ -0,0 +1,9 @@
+
+public class JavaGame2020 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Antotnio hat scheiﬂe in der Grube gemacht");
+	}
+
+}
